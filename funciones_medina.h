e915c0be069c099ef8b4d08129419de59697ad94
Pablo_Bravo <pablo.97.casla@gmail.com>
2024-09-21 23:10:28 -0300

    funciones_medina.h

--- funciones_medina.h
+++ funciones_medina.h
@@ -1,15 +1,21 @@
 #ifndef FUNCIONES_MEDINA_H_INCLUDED
 #define FUNCIONES_MEDINA_H_INCLUDED
 
-#include "funciones_bravo.h"
+typedef struct
+{
+    bool flag;
+    tHeader header;
+    tHeaderExt headerExt;
+    tPixel** matriz;
+} tDatosBMP;
 
-void escalaGrises(tDatosBMP* Imagen);
-void invertirHorizontal(tDatosBMP* Imagen);
-void invertirVertical(tDatosBMP* Imagen);
-void negativo(tDatosBMP* Imagen);
-void recortar(tDatosBMP* Imagen, tArgumento* argumento);
-void achicar(tDatosBMP* Imagen, tArgumento* argumento);
-void concatenarHorizontal(tDatosBMP* Imagen1, tDatosBMP* Imagen2);
-void concatenarVertical(tDatosBMP* Imagen1, tDatosBMP* Imagen2);
+tDatosBMP datoImagen (const char []);
+
+void achicar (tDatosBMP,char [],int);
+void recortar (tDatosBMP,char [],int);
+void espejarHorizontal (tDatosBMP,char []);
+void espejarVertical (tDatosBMP,char []);
+void escalaDeGrises (tDatosBMP,char []);
+void comodin (tDatosBMP,char []);
 
 #endif // FUNCIONES_MEDINA_H_INCLUDED
